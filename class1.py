b0b47cc9d3f8140d103da3ca09a411a88df04ddb
mvtiwari <mayurvtiwari@gmail.com>
2022-12-26 16:36:31 +0530

    class1.py file changed by mvtiwari

--- class1.py
+++ class1.py
@@ -2,4 +2,10 @@ class Student:
    def __init__(self, name, age):
        self.name = name
        self.age = age
+   def display(self):
+       print('Student name is : ', self.name)
+       print('Student age is :', self.age)
+       
+s1 = Student('Mayur', 30)
+s1.display()
    
